eaa6d41a87e7abca5ce9de8224c475acdb3e556f
Amanda Ackerman <ackerm44@gmail.com>
2017-10-31 20:01:12 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,10 +19,11 @@ class Author
   end
 
   def add_post_by_title(post_title)
-    post = Post.new(name)
+    post = Post.new(post_title)
     @posts << post
     post.author = self
     @@post_count += 1
+
   end
 
   def self.post_count
